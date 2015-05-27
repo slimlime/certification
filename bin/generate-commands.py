c0e29f1615a85d12f6c65fff8e1a61b2b083e291
Greg Wilson <gvwilson@third-bit.com>
2015-05-26 19:54:45 -0500

    Using names alone

--- bin/generate-commands.py
+++ bin/generate-commands.py
@@ -2,7 +2,7 @@ import sys
 import sqlite3
 import datetime
 
-QUERY = "select person.personal || ' ' || person.family, awards.awarded from person join awards on person.person=awards.person where person.person=? and awards.badge='instructor';"
+QUERY = "select person.personal || ' ' || person.family from person where person.person=?;"
 
 COMMAND = "python bin/certificates.py -i /Applications/Inkscape.app/Contents/Resources/bin/inkscape -s $PWD/instructor.svg -o $PWD/instructor/{0}.pdf date='{1}' instructor='Greg Wilson' name='{2}'"
 
@@ -12,10 +12,12 @@ cursor = connection.cursor()
 for pid in sys.stdin:
     pid = pid.strip()
     cursor.execute(QUERY, [pid])
-    name, d = cursor.fetchall()[0]
-    d = datetime.datetime(*[int(x) for x in d.split('-')])
-    d = d.strftime("%B %-e, %Y")
-    print COMMAND.format(pid, d, name)
+    try:
+        name = cursor.fetchall()[0][0]
+        d = datetime.datetime.today().strftime('%B %-e, %Y')
+        print COMMAND.format(pid, d, name)
+    except Exception, e:
+        print 'failed on {0}: {1}'.format(pid, str(e))
 
 cursor.close()
 connection.close()
